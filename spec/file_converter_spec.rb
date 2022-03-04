6ba99e2fbba1d837f23f6bea982e7c66b543104d
Chris Romero <chrisleeromero1@gmail.com>
2022-03-04 14:20:35 -0700

    Refactor: refactor first initialize test and add passing code via a new file_converter class

--- spec/file_converter_spec.rb
+++ spec/file_converter_spec.rb
@@ -1,13 +1,13 @@
 require 'simplecov'
 SimpleCov.start
-require './lib/english_to_braille'
+require './lib/file_converter'
 
 RSpec.describe FileConverter do
 
   describe 'Iteration 1' do
 
-    xit "Is instantiated as an object" do
-      translation = New_.new
+    it "Is instantiated as an object" do
+      translation = FileConverter.new(File.read("./lib/message.txt"))
       expect(translation).to be_a(FileConverter)
       # expect(translation.message).to eq("Created 'braille.txt' containing 256 characters")
     end

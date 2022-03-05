845ce7cde331c6d89af40228903312f937d596ea
Chris Romero <chrisleeromero1@gmail.com>
2022-03-05 00:19:56 -0700

    Refactor: add update method to runner file to convert english letter to braille

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,4 +2,5 @@ require './lib/file_converter'
 
 translation = FileConverter.new(File.read("./lib/message.txt"))
 translation.create_new_file
+translation.update_new_file_to_braille
 translation.print_confirmation

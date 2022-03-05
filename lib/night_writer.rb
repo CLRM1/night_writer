453d097f06a9349313b9b57431f03d3ad168589b
Chris Romero <chrisleeromero1@gmail.com>
2022-03-04 18:14:07 -0700

    Refactor: install rubocop and update syntax based on suggested corrections

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,4 +2,4 @@ require './lib/file_converter'
 
 translation = FileConverter.new(File.read("./lib/message.txt"))
 translation.create_new_file
-translation.read_new_file
+translation.print_confirmation
--- spec/file_converter_spec.rb
+++ spec/file_converter_spec.rb
@@ -3,38 +3,35 @@ SimpleCov.start
 require './lib/file_converter'
 
 RSpec.describe FileConverter do
-
   before(:each) do
-    @file = "./lib/message.txt"
-    @file_2 = "braille.txt"
-    # @translation = FileConverter.new(File.read(@file))
+    @file = './lib/message.txt'
+    @new_file = 'braille.txt'
+    @translation = FileConverter.new(File.read(@file))
   end
 
   describe 'Iteration 1' do
-
-    xit "Is instantiated as an object" do
+    it 'Is instantiated as an object' do
       expect(@translation).to be_a(FileConverter)
     end
 
-    xit "can read a file" do
+    it 'can read a file' do
       # require 'pry'; binding.pry
       expect(@translation.file.lines[1]).to eq("hello world!\n")
     end
 
-    it "can create a new file with the same contents as the first" do
+    it 'can create a new file with the same contents as the first' do
       # require 'pry'; binding.pry
-      ARGV = [@file, @file_2]
+      ARGV = [@file, @new_file]
       translation = FileConverter.new(File.read(@file))
       translation.create_new_file
-      expect(translation.new_braille_file.lines[1]).to eq("hello world!\n")
+      expect(translation.read_new_file[1]).to eq("hello world!\n")
     end
 
-    xit "can read the new file" do
-      ARGV = [@file, @file_2]
+    it 'can read the file and print confirmation to the terminal' do
+      ARGV = [@file, @new_file]
       @translation = FileConverter.new(File.read(@file))
       @translation.create_new_file
-      expect(@translation.read_new_file).to eq("Created 'braille.txt' containing 256 characters")
+      expect(@translation.print_confirmation).to eq("Created 'braille.txt' containing 13 characters")
     end
-
   end
 end

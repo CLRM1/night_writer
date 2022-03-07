da87f7666b92b49a09b2bcfba3506d9ec8d41f7e
Chris Romero <chrisleeromero1@gmail.com>
2022-03-06 23:35:44 -0700

    Refactor: refactor update_new_file_to_braille method to handle nil case

--- lib/english_braille_map.rb
+++ lib/english_braille_map.rb
@@ -13,42 +13,42 @@ class English_Braille_Map
   end
 
   def letters_a_through_g
-    @english_key_map[:a] = ['0 .','. .','. .']
-    @english_key_map[:b] = ['0 .','0 .','. .']
-    @english_key_map[:c] = ['0 0','. .','. .']
-    @english_key_map[:d] = ['0 0','. 0','. .']
-    @english_key_map[:e] = ['0 .','. 0','. .']
-    @english_key_map[:f] = ['0 0','0 .','. .']
-    @english_key_map[:g] = ['0 0','0 0','. .']
+    @english_key_map[:a] = ['0.','..','..']
+    @english_key_map[:b] = ['0.','0.','..']
+    @english_key_map[:c] = ['00','..','..']
+    @english_key_map[:d] = ['00','.0','..']
+    @english_key_map[:e] = ['0.','.0','..']
+    @english_key_map[:f] = ['00','0.','..']
+    @english_key_map[:g] = ['00','00','..']
   end
 
   def letters_h_through_n
-    @english_key_map[:h] = ['0 .','0 0','. .']
-    @english_key_map[:i] = ['. 0','0 .','. .']
-    @english_key_map[:j] = ['. 0','0 0','. .']
-    @english_key_map[:k] = ['0 .','. .','0 .']
-    @english_key_map[:l] = ['0 .','0 .','0 .']
-    @english_key_map[:m] = ['0 0','. .','0 .']
-    @english_key_map[:n] = ['0 0','. 0','0 .']
+    @english_key_map[:h] = ['0.','00','..']
+    @english_key_map[:i] = ['.0','0.','..']
+    @english_key_map[:j] = ['. 0','00','..']
+    @english_key_map[:k] = ['0.','..','0.']
+    @english_key_map[:l] = ['0.','0.','0.']
+    @english_key_map[:m] = ['00','..','0.']
+    @english_key_map[:n] = ['00','. 0','0.']
   end
 
   def letters_o_through_u
-    @english_key_map[:o] = ['0 .','. 0','0 .']
-    @english_key_map[:p] = ['0 0','0 .','0 .']
-    @english_key_map[:q] = ['0 0','0 0','0 .']
-    @english_key_map[:r] = ['0 .','0 0','0 .']
-    @english_key_map[:s] = ['. 0','0 .','0 .']
-    @english_key_map[:t] = ['. 0','0 0','0 .']
-    @english_key_map[:u] = ['0 .','. .','0 0']
+    @english_key_map[:o] = ['0.','. 0','0.']
+    @english_key_map[:p] = ['00','0.','0.']
+    @english_key_map[:q] = ['00','00','0.']
+    @english_key_map[:r] = ['0.','00','0.']
+    @english_key_map[:s] = ['.0','0.','0.']
+    @english_key_map[:t] = ['.0','00','0.']
+    @english_key_map[:u] = ['0.','..','00']
   end
 
   def letters_v_through_z_and_space
-    @english_key_map[:v] = ['0 .','0 .','0 0']
-    @english_key_map[:w] = ['. 0','0 0','. 0']
-    @english_key_map[:x] = ['0 0','. .','0 0']
-    @english_key_map[:y] = ['0 0','. 0','0 0']
-    @english_key_map[:z] = ['0 .','. 0','0 0']
-    @english_key_map[:" "] = ['. .','. .','. .']
+    @english_key_map[:v] = ['0 .','0 .','00']
+    @english_key_map[:w] = ['.0','00','.0']
+    @english_key_map[:x] = ['00','..','00']
+    @english_key_map[:y] = ['00','.0','00']
+    @english_key_map[:z] = ['0 .','.0','00']
+    @english_key_map[:" "] = ['..','..','..']
   end
 
   def all_letters
